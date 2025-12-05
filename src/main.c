50800b94e9827e6a61dd4bc32035a7f1d1a7e40e
Ryan OConnor <ryanoconnor.dev@gmail.com>
2025-12-04 19:42:29 -0500

    add original dos font

--- src/main.c
+++ src/main.c
@@ -40,7 +40,13 @@ int main() {
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
     // Load a font (make sure the path is correct)
-    TTF_Font *font = TTF_OpenFont("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 24);
+    //TTF_Font *font = TTF_OpenFont("/usr/share/fonts/truetype/dejavu/DejaVuSansMono-Bold.ttf", 64);
+    TTF_Font* font = TTF_OpenFont("../assets/fonts/Px437_IBM_VGA_8x16.ttf", 64);
+
+//     char cwd[512];
+// getcwd(cwd, sizeof(cwd));
+// printf("Working dir: %s\n", cwd);
+
     if (!font) {
         printf("TTF_OpenFont Error: %s\n", TTF_GetError());
         SDL_DestroyRenderer(renderer);
@@ -50,9 +56,20 @@ int main() {
         return 1;
     }
 
-    SDL_Color color = {255, 255, 255, 255}; // white
 
-    SDL_Surface *surface = TTF_RenderText_Solid(font, "Hello, Gorillas!", color);
+
+    char *intro_txt1 = "Copyright (C) CorpHackRyan Corporation 2025";
+    char *intro_txt2 = "our mission is to hit your opponent with the exploding";
+    char *intro_txt3 = "banana by varying the angle and power of your throw, taking";
+    char *intro_txt4 = "into account wind speed, gravity, and the city skyline.";
+    char *intro_txt5 = "The wind speed is shown by a directional arrow at the bottom";
+    char *intro_txt6 = "of the playing field, its length relative to its strength.";
+    char *intro_txt7 = "Press any key to continue";
+
+    SDL_Color color = {255, 255, 0, 255}; // white
+
+
+    SDL_Surface *surface = TTF_RenderText_Solid(font, intro_txt1, color);
     SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_FreeSurface(surface);
 
@@ -63,7 +80,6 @@ int main() {
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_QUIT) running = 0;
         }
-
         SDL_SetRenderDrawColor(renderer, 0, 0, 128, 255); // dark blue background
         SDL_RenderClear(renderer);
 
@@ -72,6 +88,7 @@ int main() {
         SDL_RenderCopy(renderer, texture, NULL, &dst);
 
         SDL_RenderPresent(renderer);
+        
     }
 
     SDL_DestroyTexture(texture);
